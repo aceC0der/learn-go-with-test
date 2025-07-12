ce84b1b2c5bbd6c6314d6ae8976b78be27f459e3
aceC0der <paulmridul.mp@gmail.com>
2025-07-13 02:57:58 +0600

    feat(#1): added reflect chapter code and tests

---
+++ go-fundamentals/reflection/reflection.go
@@ -0,0 +1,31 @@
+package reflection
+
+import (
+	"reflect"
+)
+
+func walk(x any, fn func(input string)) {
+	val := getValue(x)
+	
+	switch val.Kind(){
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+	case reflect.String:
+		fn(val.String())
+	}
+}
+
+func getValue(x any) reflect.Value {
+	val := reflect.ValueOf(x)
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	return val
+}
